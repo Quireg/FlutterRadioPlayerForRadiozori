ce8fb4bdc1ae8d0a393b02e3cd18f9953a6767bf
Artur Menchenko <artur.menchenko@gmail.com>
2024-01-17 20:39:01 +0200

    Radiozori player implementation

---
+++ radiozori22/lib/themes/text_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:radiozori22/screen/size_extension.dart';
+
+import 'colors.dart';
+
+
+TextStyle get textHeading => TextStyle(fontSize: 12.sp, color: AppColors.textFont);
+TextStyle get textNormal => TextStyle(fontSize: 8.sp, color: AppColors.textFont);
